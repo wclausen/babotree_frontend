71c25c5b4955ecc91d0f3f15b946f5985ba93996
Will Clausen <william.h.f.clausen@gmail.com>
2024-01-03 23:32:22 -0500

    updates

---
+++ src/routes/Flashcard.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { Card } from 'flowbite-svelte';
+    export let question;
+    export let answer;
+</script>
+
+<Card class="h-full">
+    <div class="max-w-xl p-4">{question}</div>
+    <hr>
+    <div class="max-w-xl p-4">{answer}</div>
+</Card>
